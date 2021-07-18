bb1df7157485b942840320b27313f0fbc33c9d8d
andxras <cl.bs.xv@gmail.com>
2021-07-18 06:34:12 -0400

    Updates

---
+++ component---src-pages-404-js-06ddbf4eb5fe04fa9dae.js
@@ -0,0 +1,2 @@
+(self.webpackChunkportfolio=self.webpackChunkportfolio||[]).push([[883],{9616:function(e,t,r){"use strict";r.r(t);var n=r(7294),o=r(5444);t.default=function(){return n.createElement(n.Fragment,null,n.createElement("title",null,"Error 404 Page Not found"),n.createElement(o.rU,{to:"/"},"Go home"),".")}}}]);
+//# sourceMappingURL=component---src-pages-404-js-06ddbf4eb5fe04fa9dae.js.map
