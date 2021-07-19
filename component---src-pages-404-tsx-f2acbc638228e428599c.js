9d9206569ae7ddecc02bc6aded15171a966a6945
andxras <cl.bs.xv@gmail.com>
2021-07-19 13:27:12 -0400

    Updates

---
+++ component---src-pages-404-tsx-f2acbc638228e428599c.js
@@ -0,0 +1,2 @@
+(self.webpackChunkportfolio=self.webpackChunkportfolio||[]).push([[218],{938:function(e,t,r){"use strict";r.r(t);var n=r(7294),o=r(5444);t.default=function(){return n.createElement(n.Fragment,null,n.createElement("title",null,"Error 404 Page Not found"),n.createElement(o.rU,{to:"/"},"Go home"),".")}}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-f2acbc638228e428599c.js.map
