c4fc72bffc16a5651b839ace2a1041a56dc693c2
andxras <cl.bs.xv@gmail.com>
2021-07-22 15:15:54 -0400

    lint-staged test

--- src/components/showcase/index.tsx
+++ src/components/showcase/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import { Link } from 'gatsby'
-import { Section } from "./styles"
+import { Section } from './styles'
 
 // data
 import data from '../../../data.json'
