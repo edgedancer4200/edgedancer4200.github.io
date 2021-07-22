0a9edc1cccacbb193e8c816ff305eff5811935e5
andxras <cl.bs.xv@gmail.com>
2021-07-22 12:36:33 -0400

    eslint and prettier added

--- src/devices-bpks.ts
+++ src/devices-bpks.ts
@@ -1,10 +1,3 @@
-// Default
-// xs: '0px',
-// sm: '576px',
-// md: '768px',
-// lg: '992px',
-// xl: '1200px'
-
 const size = {
     mobileXS: '250px',
     mobileS: '320px',
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,37 +1,39 @@
-import React, { useEffect } from "react";
+import React, { useEffect } from 'react'
 
-import { Helmet } from "react-helmet"
+import { Helmet } from 'react-helmet'
 
 // local components
-import Navigation from '../components/navigation';
-import ProjectsSection from '../components/projects_section';
-import Footer from '../components/footer';
-import Showcase from '../components/showcase';
-import Contact from '../components/contact';
+import Navigation from '../components/navigation'
+import ProjectsSection from '../components/projects_section'
+import Footer from '../components/footer'
+import Showcase from '../components/showcase'
+import Contact from '../components/contact'
 
 // styles
-import '../assets/global.css';
-
+import '../assets/global.css'
 
 const IndexPage = () => {
   useEffect(() => {
-		window.scrollTo(0,0);
-	}, []);
+    window.scrollTo(0, 0)
+  }, [])
 
   return (
     <>
       <Helmet htmlAttributes={{
-        lang: 'en',
+        lang: 'en'
       }}>
         <meta name="description" content="Carlos Barrios, developer" charSet="utf-8"/>
         <title>
         ॐ Carlos Barrios, developer
         </title>
       </Helmet>
-      
+
       <Navigation/>
       <Showcase/>
       <ProjectsSection/>
+      <Contact/>
+      <Footer/>
+
       <Contact/>
       <Footer/>
     </>
